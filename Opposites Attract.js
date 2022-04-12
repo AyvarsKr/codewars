7bc07444b73cf76db958756439033dbafc2fabdd
Ayvars Krizhus <Ayvars@bk.ru>
2022-04-13 00:14:30 +0300

    task from 13.04

--- Opposites Attract.js
+++ Opposites Attract.js
@@ -1,8 +1,11 @@
 function lovefunc(flower1, flower2){
-    if(flower1 % 2 && flower2 !%2) {
-        console.log('est')
-    }
-
-  }
-  
-  lovefunc(2, 1)
+    if(flower1 % 2 == 0 && flower2 % 2 == 1){
+       return true;
+   }
+    if(flower1 % 2 == 1 && flower2 % 2 == 0){
+      return true;
+   }
+   else{
+       return false;
+       }
+   }
